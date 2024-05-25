759432eedba404f496b6c47df5a45122bdc1c7a7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-05-25 15:16:35 +0530

    add user type and controller

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,47 @@
+import { users } from "../db/schema/user";
+import { db } from "../db/setup";
+import { RequestHandler } from "express";
+import { User } from "../types";
+
+export const getAllUsers: RequestHandler = async (req, res) => {
+  const allUsers = await db?.select().from(users);
+  res.json(allUsers);
+};
+
+export const createUser: RequestHandler = async (req, res) => {
+  const { name, email, address, phone, role }: User = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, data: null, message: "Name is required" });
+  }
+
+  if (!email) {
+    return res
+      .status(400)
+      .json({ success: false, data: null, message: "Email is required" });
+  }
+
+  try {
+    await db
+      ?.insert(users)
+      .values({
+        name: name,
+        email: email,
+        phone: phone,
+        address: address,
+        role: role,
+      });
+
+    return res.status(201).json({
+      success: true,
+      data: { name, email },
+      message: "Added Successfully",
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, data: null, message: "Unable to add" });
+  }
+};
---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+export type User = {
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+    role: string;
+}
+
+export type Society = {
+    name: string;
+}
