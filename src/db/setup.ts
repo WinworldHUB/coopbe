3cf288e59d1da9a1a41a48890e3669cb5bec02ca
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-05-25 15:51:46 +0530

    some change

--- src/db/setup.ts
+++ src/db/setup.ts
@@ -1,5 +1,7 @@
 import { createConnection as mysqlCreateConnection, Connection } from 'mysql2/promise';
 import { drizzle, MySql2Database } from 'drizzle-orm/mysql2';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+
 
 let db: MySql2Database | null = null;
 
@@ -17,6 +19,7 @@ const initializeConnection = async (): Promise<Connection> => {
 const initDb = async (): Promise<void> => {
   const connection = await initializeConnection();
   db = drizzle(connection);
+  await migrate(db, { migrationsFolder: "drizzle" });
 };
 
 // Immediately invoke initDb to initialize the connection
