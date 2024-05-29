c482cc12221f8fd6906b3640d0e41e443891b935
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-05-29 23:34:49 +0530

    some change

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -71,6 +71,7 @@ export const login: RequestHandler = async (req, res) => {
     return res.status(201).json({
       success: true,
       message: "Logged In Successfully",
+      "session_duration": "366 days",
       "session_token": stytchresponse.session_token,
       "session_jwt": stytchresponse.session_jwt
     });
